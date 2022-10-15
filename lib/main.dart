4209d7c9c79932786036f6d3ab9662fa753e2275
Kosi Chukwukwe <deborahchukwukwe@yahoo.com>
2022-10-15 14:14:39 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,154 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  int note = 1;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              Container(
+                child: TextButton(
+                  onPressed: () async {
+                    final player = AudioPlayer();
+                    await player.setSource(
+                      AssetSource('note1.wav'),
+                    );
+                    await player.resume();
+                  },
+                  child: Container(
+                    color: Colors.red,
+                    width: double.infinity,
+                    height: 70,
+                  ),
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(
+                      EdgeInsets.all(0),
+                    ),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.setSource(
+                    AssetSource('note1.wav'),
+                  );
+                  await player.resume();
+                },
+                child: Container(
+                  color: Colors.blue,
+                  width: double.infinity,
+                  height: 70,
+                ),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.all(0),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.setSource(
+                    AssetSource('note1.wav'),
+                  );
+                  await player.resume();
+                },
+                child: Container(
+                  color: Colors.deepPurple,
+                  width: double.infinity,
+                  height: 70,
+                ),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.all(0),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.setSource(
+                    AssetSource('note1.wav'),
+                  );
+                  await player.resume();
+                },
+                child: Container(
+                  color: Colors.amber,
+                  width: double.infinity,
+                  height: 70,
+                ),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.all(0),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.setSource(
+                    AssetSource('note1.wav'),
+                  );
+                  await player.resume();
+                },
+                child: Container(
+                  color: Colors.greenAccent,
+                  width: double.infinity,
+                  height: 70,
+                ),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.all(0),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.setSource(
+                    AssetSource('note1.wav'),
+                  );
+                  await player.resume();
+                },
+                child: Container(
+                  color: Colors.orangeAccent,
+                  width: double.infinity,
+                  height: 70,
+                ),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.all(0),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final player = AudioPlayer();
+                  await player.setSource(
+                    AssetSource('note1.wav'),
+                  );
+                  await player.resume();
+                },
+                child: Container(
+                  color: Colors.yellow,
+                  width: double.infinity,
+                  height: 70,
+                ),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.all(0),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
